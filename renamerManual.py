7ab371705170cedbfec8fc679eb5cfe47b9ffe07
treehann <jtbswartz@yahoo.com>
2023-08-20 16:47:28 -0400

    renamerManual

---
+++ renamerManual.py
@@ -0,0 +1,12 @@
+import os
+
+def replace_character_in_filenames(directory, old_char, new_char):
+    for filename in os.listdir(directory):
+        if old_char in filename:
+            new_filename = filename.replace(old_char, new_char)
+            os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename))
+
+if __name__ == "__main__":
+    old_char = input("Enter the character to be replaced: ")
+    new_char = input("Enter the character to replace it with: ")
+    replace_character_in_filenames(".", old_char, new_char)

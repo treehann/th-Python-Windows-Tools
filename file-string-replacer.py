e6b03b9c97cc2541e8daf5c611ec13c702720b0a
treehann <jtbswartz@yahoo.com>
2023-09-10 13:23:03 -0400

    rename tools for clarity

--- file-string-replacer.py
+++ file-string-replacer.py
@@ -7,6 +7,6 @@ def replace_character_in_filenames(directory, old_char, new_char):
             os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename))
 
 if __name__ == "__main__":
-    old_char = input("Enter the character to be replaced: ")
-    new_char = input("Enter the character to replace it with: ")
+    old_char = input("Enter the string to be replaced: ")
+    new_char = input("Enter the string to replace it with: ")
     replace_character_in_filenames(".", old_char, new_char)
